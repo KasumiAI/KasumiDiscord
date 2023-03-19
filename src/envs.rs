711c6fb519f6acaac882833c67855cd483dd4216
Aleksey Pestov <a.pestov@datasakura.com>
2023-03-19 21:00:19 +0300

    clippy fixes

--- src/envs.rs
+++ src/envs.rs
@@ -2,20 +2,11 @@ use std::env;
 
 use once_cell::sync::Lazy;
 
-pub static OPENAI_KEY: Lazy<String> = Lazy::new(|| {
-    env::var("OPENAI_KEY")
-        .expect("Expected a OPENAI_KEY in the environment")
-        .to_string()
-});
+pub static OPENAI_KEY: Lazy<String> =
+    Lazy::new(|| env::var("OPENAI_KEY").expect("Expected a OPENAI_KEY in the environment"));
 
-pub static DISCORD_TOKEN: Lazy<String> = Lazy::new(|| {
-    env::var("DISCORD_TOKEN")
-        .expect("Expected a DISCORD_TOKEN in the environment")
-        .to_string()
-});
+pub static DISCORD_TOKEN: Lazy<String> =
+    Lazy::new(|| env::var("DISCORD_TOKEN").expect("Expected a DISCORD_TOKEN in the environment"));
 
-pub static DATABASE_URL: Lazy<String> = Lazy::new(|| {
-    env::var("DATABASE_URL")
-        .expect("Expected a DATABASE_URL in the environment")
-        .to_string()
-});
+pub static DATABASE_URL: Lazy<String> =
+    Lazy::new(|| env::var("DATABASE_URL").expect("Expected a DATABASE_URL in the environment"));
